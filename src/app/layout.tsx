4aa6d617f4d6f0429117d950d7dbb9f75ec87cfa
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-08-20 09:58:21 +0700

    feat: add opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Gerakan Infaq Beras Tasikmalaya",
   description: "Gerakan Infaq Beras Tasikmalaya",
+  openGraph: {
+    title: 'Gerakan Infaq Beras Tasikmalaya',
+    description: 'Bantu Yatim & Penghafal Qur\'an bersama Gerakan Infaq Beras Gerakan Infaq Beras Tasikmalaya',
+    url: 'https://gib-tasikmalaya.vercel.app',
+    siteName: 'Gerakan Infaq Beras Tasikmalaya',
+    images: [
+      {
+        url: 'https://gib-tasikmalaya.vercel.app/images/opengraph.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
